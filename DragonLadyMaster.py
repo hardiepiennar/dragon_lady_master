c4f933474767633d663fdbb539e47ac59a12cbdb
Hardie Pienaar <hardiepienaar@gmail.com>
2016-07-04 19:00:53 +0000

    initial commit
    Signed-off-by: Hardie Pienaar <hardiepienaar@gmail.com>

---
+++ DragonLadyMaster.py
@@ -0,0 +1,52 @@
+"""
+Dragon Lady master code. Dragon lady is a ndvi camera consisting of a master and slave raspberry pi. Each raspberry pi has its own camera. The master is connected to the PiNOIR camera while the slave is connected to the RGB camera.
+
+Hardie Pienaar
+June 2016
+"""
+
+# Import dependencies
+import picamera
+import time
+
+# Setup GPIO pins
+#TODO
+
+# Set status to busy (Red)
+#TODO
+
+# Read camera settings file
+#TODO
+
+# Set camera settings
+#TODO
+camera = picamera.PiCamera()
+camera.resolution = (2592, 1944)
+# camera.shutter_speed
+# camera.iso
+# camera.analog_gain
+# camera.digital_gain
+# camera.exposure_mode = 'off'
+# camera.awb_mode = 'off'
+# camera.awb_gains red,blue
+
+# Wait for slave ready
+#TODO
+
+# Set state to idle (Green)
+#TODO
+
+# Wait for camera trigger 
+#TODO
+
+# Set state to shutter (Blue)
+#TODO
+
+# Send trigger to slave 
+#TODO
+
+# Take photo 
+camera.capture("foo.jpg")
+
+# Wait for trigger
+#TODO
